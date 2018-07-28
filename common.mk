37fe131280815a752c4a3967aad6b402ab5519d5
Martin Errenst <imp@hannover.ccc.de>
2018-07-28 21:50:27 +0200

    add "test-wo-flash" target to avoid problems with the boot-mode status after flashing

--- common.mk
+++ common.mk
@@ -260,6 +260,9 @@ size: $(PROGRAM_OUT)
 test: flash
 	$(FILTEROUTPUT) --port $(ESPPORT) --baud 115200 --elf $(PROGRAM_OUT)
 
+test-wo-flash:
+	$(FILTEROUTPUT) --port $(ESPPORT) --baud 115200 --elf $(PROGRAM_OUT)
+
 # the rebuild target is written like this so it can be run in a parallel build
 # environment without causing weird side effects
 rebuild:
